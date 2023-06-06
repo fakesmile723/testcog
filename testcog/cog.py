1d344d829ee6c199f3001cc1fffbd2ff3da9ab1c
fakesmile723 <69390477+fakesmile723@users.noreply.github.com>
2023-06-06 14:24:52 +0530

    Update cog.py

--- testcog/cog.py
+++ testcog/cog.py
@@ -1,13 +1,30 @@
 from redbot.core import commands
+from redbot.core.bot import Red
+import discord
+
+class View(discord.ui.View):
+    def __init__(self) -> None:
+        super().__init__(timeout=180)  # seconds
+        self._message: discord.Message = None
+
+    async def on_timeout(self) -> None:
+        if self._message is not None:
+            self.click_here_button.disabled = True
+            try:
+                await self._message.edit(view=self)
+            except discord.HTTPException:
+                pass
+
+    @discord.ui.button(label="Click here.", style=discord.ButtonStyle.success)
+    async def click_here_button(self, interaction: discord.Interaction, button: discord.ui.Button) -> None:
+        await interaction.response.send_message("You clicked on the button.", ephemeral=True)
 
 class Cog(commands.Cog):
-    """My custom cog"""
-
-    def __init__(self, bot):
+    def __init__(self, bot: Red) -> None:
         self.bot = bot
 
     @commands.command()
-    async def mycom(self, ctx):
-        """This does stuff!"""
-        # Your code will go here
-        await ctx.send("I can do stuff!")
+    async def sendbutton(self, ctx: commands.Context) -> None:
+        embed: discord.Embed = discord.Embed(title="Click on the button below.", color=await ctx.embed_color())
+        view = View()
+        view._message = await ctx.send(embed=embed, view=view)

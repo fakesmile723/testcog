40105c2db47d8bdffd7d5d1cd7e86aa810747276
fakesmile723 <69390477+fakesmile723@users.noreply.github.com>
2023-06-11 21:25:41 +0530

    Update cog.py

--- testcog/cog.py
+++ testcog/cog.py
@@ -1,59 +1,13 @@
-# from redbot.core import commands
-# from redbot.core.bot import Red
-# import discord
-
-# class VerifyView(discord.ui.View):
-#     def __init__(self, role_id: int, invoker_id: int) -> None:
-#         super().__init__()
-#         self.role_id = role_id
-#         self.invoker_id = invoker_id
-
-#     @discord.ui.button(label="Add Role", style=discord.ButtonStyle.success)
-#     async def add_role_button(self, button: discord.ui.Button, interaction: discord.Interaction) -> None:
-#         member = interaction.user
-#         role = interaction.guild.get_role(self.role_id)
-#         if role and role not in member.roles:
-#             await member.add_roles(role)
-#             await interaction.response.send_message("Role added!", ephemeral=True)
-#         else:
-#             await interaction.response.send_message("You already have the role.", ephemeral=True)
-
-#     @discord.ui.button(label="Try Again", style=discord.ButtonStyle.secondary)
-#     async def try_again_button(self, button: discord.ui.Button, interaction: discord.Interaction) -> None:
-#         await interaction.response.send_message("Please try again.", ephemeral=True)
-
-#     async def interaction_check(self, interaction: discord.Interaction) -> bool:
-#         return interaction.user.id == self.invoker_id
-
-# class Cog(commands.Cog):
-#     def __init__(self, bot: Red) -> None:
-#         self.bot = bot
-
-#     @commands.command()
-#     async def sendbutton(self, ctx: commands.Context) -> None:
-#         role_id = 959146966858752006  # Modify this with the desired role ID
-#         invoker_id = ctx.author.id
-#         embed = discord.Embed(title="Verify using Discord", color=await ctx.embed_color())
-#         view = VerifyView(role_id, invoker_id)
-#         view.bot = self.bot  # Add this line to assign the bot attribute
-#         view.add_item(discord.ui.Button(style=discord.ButtonStyle.success, label="Verify"))
-#         await ctx.send(embed=embed, view=view)
-import discord
-from redbot.core.bot import Red
 from redbot.core import commands
 
-class Pupu(commands.Cog):
+class MyCog(commands.Cog):
     """My custom cog"""
 
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
-    async def pupu(self, ctx):
+    async def mycom(self, ctx):
         """This does stuff!"""
         # Your code will go here
         await ctx.send("I can do stuff!")
-    @commands.hybrid_command()
-    async def pupu(ctx):
-        await ctx.send(f"nono", ephemeral=True)
-        
